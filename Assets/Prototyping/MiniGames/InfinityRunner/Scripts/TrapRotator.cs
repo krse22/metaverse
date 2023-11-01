66869eb794644172fe60eb9fb10102e9accd3cfc
krsevan <krsevan@DESKTOP-MGS7V02>
2023-11-01 18:01:47 +0100

    More traps

---
+++ Assets/Prototyping/MiniGames/InfinityRunner/Scripts/TrapRotator.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class TrapRotator : MonoBehaviour
+{
+
+    [SerializeField] private float rotateSpeed;
+
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + rotateSpeed * Time.deltaTime);
+    }
+}
