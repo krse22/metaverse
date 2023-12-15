20be67aa6b5b03968cf1a95f6f1cff8ef9be815d
krsevan <krsevan@DESKTOP-MGS7V02>
2023-12-15 23:35:59 +0100

    Tower Defense screen

---
+++ Assets/Prototyping/MiniGames/Tower defense/Scripts/TowerDefenseCameraController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+
+namespace Prototyping.Games
+{
+    public class TowerDefenseCameraController : MonoBehaviour, ICameraHolder
+    {
+
+        [SerializeField] private GameObject top;
+        [SerializeField] private GameObject bottom;
+
+        [SerializeField] private GameObject left;
+        [SerializeField] private GameObject right;
+
+        [SerializeField] private Vector3 eulerRotation;
+
+        void Start()
+        {
+            PlayerCoreCamera.SetCameraOwner(this);
+        }
+
+        public (Vector3, Vector3) PositionAndRotation()
+        {
+            Vector3 originRight = Camera.main.WorldToScreenPoint(right.transform.position);
+            Vector3 originTop = Camera.main.WorldToScreenPoint(top.transform.position);
+
+            float y = Camera.main.transform.position.y;
+
+            if (Screen.height < originTop.y || Screen.width < originRight.x)
+            {
+                y += 25f * Time.deltaTime;
+            }
+
+            return (new Vector3(0f, y, 0f), eulerRotation);
+        }
+    }
+}
+
